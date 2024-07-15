923cb36f1fcd4f706b9b42ac291024a9742ef1b6
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2024-07-15 21:19:19 +0300

    make config loading

---
+++ cmd/sso/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// init config
+
+	// init logger
+
+	// init storage
+
+	// init application
+
+}
---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env      string `yaml:"env"`
+	DSN      string `yaml:"dsn"`
+	GRPC     `yaml:"grpc"`
+	TokenTTL time.Duration `yaml:"token_ttl"`
+}
+
+type GRPC struct {
+	Port int `yaml:"port"`
+}
+
+func MustLoad() *Config {
+	p := fetchPath()
+	c := Config{}
+	if err := cleanenv.ReadConfig(p, &c); err != nil {
+		panic(err.Error())
+	}
+
+	return &c
+}
+
+func fetchPath() string {
+	var path string
+	flag.StringVar(&path, "cfg", "", "path to service config")
+	flag.Parse()
+	if path != "" {
+		return path
+	}
+	path = os.Getenv("SSO_CONFIG_PATH")
+	if path != "" {
+		return path
+	}
+
+	panic("path to config file is not defined")
+}
